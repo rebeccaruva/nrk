3b002d13e91ddb3af7b5886780acf907b7fbc212
Nick Florez <demouser@MacBook-Air-5.local>
2016-07-25 15:54:25 -0700

    update

--- main.py
+++ main.py
@@ -29,11 +29,30 @@ class LoginHandler(webapp2.RequestHandler):
         self.response.out.write('<html><body>%s</body></html>' % greeting)
 
 
-
 class TimeLineHandler(webapp2.RequestHandler):
     def get(self):
         template = jinja_env.get_template('checked.html')
         self.response.write(template.render())
+
+#class AddEventHandler(webapp2.RequestHandler):
+
+
+#     def job():
+#         print("I'm working...")
+#
+# schedule.every(10).minutes.do(job)
+# schedule.every().hour.do(job)
+# schedule.every().day.at("10:30").do(job)
+# schedule.every().monday.do(job)
+# schedule.every().wednesday.at("13:15").do(job)
+#
+#     def get (self):
+#         self.response.out.write("example return!!")
+#
+# while True:
+#     schedule.run_pending()
+#     time.sleep(1)
+
 
 class AddEventHandler(webapp2.RequestHandler):
     def get(self):
