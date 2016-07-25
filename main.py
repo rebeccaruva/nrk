0b7b2b16d6b9164e1c9e43e005f1f1346f8eeff8
Nick Florez <demouser@MacBook-Air-5.local>
2016-07-25 09:40:08 -0700

    added background for register

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ import os
 import logging
 import webapp2
 
-<<<<<<< HEAD
 class LoginHandler(webapp2.RequestHandler):
     def get(self):
         user = users.get_current_user()
@@ -16,7 +15,7 @@ class LoginHandler(webapp2.RequestHandler):
             greeting = ('<a href="%s">Register with your Gmail account</a>.' %
                 users.create_login_url('/'))
         self.response.out.write('<html><body>%s</body></html>' % greeting)
-=======
+
 class TimeLineHandler(webapp2.RequestHandler):
 
 
@@ -31,11 +30,6 @@ class ListHandler(webapp2.RequestHandler):
 
 class QuoteHandler(webapp2.RequestHandler):
     def get(self,template,quote):
-
-
-class LoginHandler(webapp2.RequestHandler):
->>>>>>> 6d555e39fd2ec85204d60ba011f448f28ecf124b
-
 
 class EmailHandler(webapp2.RequestHandler):
 
