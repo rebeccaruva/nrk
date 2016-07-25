1e328122bd0e5eb97b11f0818a2c39dc71025388
Nick Florez <demouser@MacBook-Air-5.local>
2016-07-25 09:31:32 -0700

    login html

--- main.py
+++ main.py
@@ -1,27 +1,20 @@
+from google.appengine.api import users
 import json
 import jinja2
 import os
 import logging
 import webapp2
 
-class TimeLineHandler(webapp2.RequestHandler):
-
-
-class AddEventHandler(webapp2.RequestHandler):
-
-
-class ExpandListHandler(webapp2.RequestHandler):
-
-
-class ListHandler(webapp2.RequestHandler):
-
-
-class QuoteHandler(webapp2.RequestHandler):
-    def get(self,template,quote):
-
-
-class WelcomePageHandler(webapp2.RequestHandler):
-    def get(self, template):
+class LoginHandler(webapp2.RequestHandler):
+    def get(self):
+        user = users.get_current_user()
+        if user:
+            greeting = ('Welcome, %s! (<a href="%s">sign out</a>)' %
+                (user.nickname(), users.create_logout_url('/')))
+        else:
+            greeting = ('<a href="%s">Register with your Gmail account</a>.' %
+                users.create_login_url('/'))
+        self.response.out.write('<html><body>%s</body></html>' % greeting)
 
 
 
@@ -29,11 +22,7 @@ class WelcomePageHandler(webapp2.RequestHandler):
 
 jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader('templates'))
 routes = [
-  ('/time', TimeLineHandler),
-  ('/event', AddEventHandler),
-  ('/expand'), ExpandListHandler),
-  ('/list'), ListHandler),
-  ('/quote'), QuoteHandler),
-  ('/welcome'), WelcomePageHandler)
+  ('/', LoginHandler),
+
 ]
 app = webapp2.WSGIApplication(routes, debug=True)
