92c90740b83815b1e2c4e4dd1ff2ec51fcf28c19
rebeccaruva <beckyvicky@gmail.com>
2016-07-25 11:45:18 -0700

    minor changes in checked

--- main.py
+++ main.py
@@ -28,25 +28,24 @@ class LoginHandler(webapp2.RequestHandler):
         self.response.out.write('<html><body>%s</body></html>' % greeting)
 
 class TimeLineHandler(webapp2.RequestHandler):
-
-    def job():
-        print("I'm working...")
-
-schedule.every(10).minutes.do(job)
-schedule.every().hour.do(job)
-schedule.every().day.at("10:30").do(job)
-schedule.every().monday.do(job)
-schedule.every().wednesday.at("13:15").do(job)
-
     def get (self):
         self.response.out.write("example return!!")
+#     def job():
+#         print("I'm working...")
+#
+# schedule.every(10).minutes.do(job)
+# schedule.every().hour.do(job)
+# schedule.every().day.at("10:30").do(job)
+# schedule.every().monday.do(job)
+# schedule.every().wednesday.at("13:15").do(job)
+#
+#     def get (self):
+#         self.response.out.write("example return!!")
+#
+# while True:
+#     schedule.run_pending()
+#     time.sleep(1)
 
-while True:
-    schedule.run_pending()
-    time.sleep(1)
-
-
-"""
 class AddEventHandler(webapp2.RequestHandler):
     def get(self):
         template = jinja_environment.get_template('addEvent.html')
