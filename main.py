0ec9ec914357fa5ce815d6bd0d7636d09a89ceae
Nick Florez <demouser@Nick.local>
2016-07-27 15:31:38 -0700

    okay

--- main.py
+++ main.py
@@ -29,6 +29,7 @@ class TimeLineHandler(webapp2.RequestHandler):
 class GifHandler(webapp2.RequestHandler):
     def get(self):
         template = jinja_environment.get_template('gif.html')
+        # template = jinja_environment.get_template('gif.html')
         response = urllib2.urlopen("http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=celebration&fmt=json")
         giphyRand = json.loads(response.read())
         gif = giphyRand["data"]["image_original_url"]
@@ -48,11 +49,26 @@ class AddEventHandler(webapp2.RequestHandler):
         checked_template = jinja_env.get_template('checked.html')
         self.response.out.write(checked_template.render())
     def post(self):
-        addEvent_template = jinja_env.get_template('addEvent.html')
-        template_variables = {
-            'main':self.request.get("main")
-            }
-        self.response.out.write(addEvent_template.render(template_variables))
+        checked_template = jinja_env.get_template('checked.html')
+        # template_variables = {
+        #     'main':self.request.get("main")
+        #     }
+        # self.response.out.write(addEvent_template.render(template_variables))
+        # template = jinja_environment.get_template('gif.html')
+        response = urllib2.urlopen("http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=celebration&fmt=json")
+        giphyRand = json.loads(response.read())
+        gif = giphyRand["data"]["image_original_url"]
+        width = giphyRand["data"]["image_width"]
+        width = int(width) + 20
+        height = giphyRand["data"]["image_height"]
+        height = int(height) + 45
+        templateVariables = {
+            'main':self.request.get("main"),
+            "gif": gif,
+            "image_width": width,
+            "image_height": height
+        }
+        self.response.out.write(checked_template.render(templateVariables))
 
 #class ExpandListHandler(webapp2.RequestHandler):
 #    def get (self):
@@ -86,7 +102,7 @@ routes = [
   ('/home', TimeLineHandler),
 #  ('/checked-list', ExpandListHandler),
 #  ('/add-event', AddEventHandler),
-  ('/addEvent', AddEventHandler),
+  ('/home', AddEventHandler),
   ('/gif', GifHandler)
 ]
 app = webapp2.WSGIApplication(routes, debug=True)
