0548e4dbb441221f7ef16ae40402935e78ca447a
Nick Florez <demouser@Nick.local>
2016-07-28 10:10:11 -0700

    okay

--- main.py
+++ main.py
@@ -76,15 +76,15 @@ class AddEventHandler(webapp2.RequestHandler):
 #        self.response.out.write("example return!!")
 
 
-class QuoteHandler(webapp2.RequestHandler):
-    def get(self):
-        template = jinja_environment.get_template('checked.html')
-        self.response.out.write(template.render())
-        response = urllib2.urlopen("http://quotes.rest/qod.json")
-        NowDict = json.loads(response.read())
-        Quote = NowDict["contents"]["quotes"][0]["quote"]
-        Author = NowDict["contents"]["quotes"][0]["author"]
-        self.response.out.write("<center>" + ' " ' + Quote + ' " ' "<br>" + "-" + Author + "</center>")
+#class QuoteHandler(webapp2.RequestHandler):
+#    def get(self):
+#        template = jinja_environment.get_template('checked.html')
+        #self.response.out.write(template.render())
+        #response = urllib2.urlopen("http://quotes.rest/qod.json")
+        #NowDict = json.loads(response.read())
+        #Quote = NowDict["contents"]["quotes"][0]["quote"]
+        #Author = NowDict["contents"]["quotes"][0]["author"]
+        #self.response.out.write("<center>" + ' " ' + Quote + ' " ' "<br>" + "-" + Author + "</center>")
         #self.response.out.write("<html><center>Quote Here</center></html>")
 
 
@@ -114,7 +114,7 @@ class AboutHandler(webapp2.RequestHandler):
 jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader('templates'))
 routes = [
   ('/', LoginHandler),
-  ('/home', QuoteHandler),
+  #('/home', QuoteHandler),
   ('/home', TimeLineHandler),
 #  ('/checked-list', ExpandListHandler),
 #  ('/add-event', AddEventHandler),
